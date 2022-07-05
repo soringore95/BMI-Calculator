9a211400d58192c488f9775c9418cb42ea33fb0c
Sorin Gore <soringore95@gmail.com>
2022-07-05 11:00:40 +0300

    Added CalculatorBrain swift file

--- BMI Calculator/Controllers/CalculateViewController.swift
+++ BMI Calculator/Controllers/CalculateViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class CalculateViewController: UIViewController {
     
-    var bmiValue = "0.0"
+    var calculatorBrain = CalculatorBrain()
 
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
@@ -40,8 +40,7 @@ class CalculateViewController: UIViewController {
         let height = heightSlider.value
         let weight = weightSlider.value
         
-        let bmi = weight / pow(height, 2)
-        bmiValue = String(format: "%.1f", bmi)
+        calculatorBrain.calculateBMI(height: height, weight: weight)
         
         self.performSegue(withIdentifier: "goToResults", sender: self)
         
@@ -50,7 +49,7 @@ class CalculateViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "goToResults" {
             let destinationVC = segue.destination as! ResultsViewController
-            destinationVC.bmiValue = bmiValue
+            destinationVC.bmiValue = calculatorBrain.getBMIValue()
           
             
         }
---
+++ BMI Calculator/Controllers/CalculatorBrain.swift
@@ -0,0 +1,22 @@
+//
+//  CalculatorBrain.swift
+//  BMI Calculator
+//
+//  Created by Sorin Gore on 05.07.2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorBrain {
+    
+    func calculateBMI(height: Float, weight: Float) {
+        let bmi = weight / (height * height)
+    }
+    
+    func getBMIValue() {
+        
+    }
+}
+
+
